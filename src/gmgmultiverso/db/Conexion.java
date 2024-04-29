8f653b132393c7bfcc57e5477e0ff598de90151c
Geanina <geany849@gmail.com>
2024-04-29 11:07:44 +0200

    Primeras configuraciones y carpetas de la bbdd

---
+++ src/gmgmultiverso/db/Conexion.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gmgmultiverso.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author geanina.foanta
+ */
+public class Conexion 
+{
+    private Connection conexion;
+    private String bbdd = "jdbc:hsqldb:hsql://localhost/";
+    private String usuario = "SA";
+    private String contrasena = "gmgmultiverso";
+    
+    /*
+    public Conexion(String bbdd, String usuario, String contrasena) throws ClassNotFoundException {
+        this.bbdd = bbdd;
+        this.usuario = usuario;
+        this.contrasena = contrasena;
+        Class.forName("com.mysql.jdbc.Driver");
+    }
+    */
+    
+    public Connection abrirConexion() throws SQLException
+    {
+        try
+        {
+            //cargar driver
+            Class.forName("org.hsqldb.jdbc.JDBCDriver");
+            
+            // Asignar la conexión a la variable de instancia para que esté disponible en toda la clase
+            this.conexion = DriverManager.getConnection(bbdd, usuario, contrasena);
+            
+            if(this.conexion != null)
+            {
+                System.out.println("Conexion exitosa");
+            }
+        }
+        catch(ClassNotFoundException | SQLException e)
+        {
+            e.printStackTrace();
+        }
+        
+        return this.conexion;
+    }
+    
+    public void cerrarConexion(Connection con) throws SQLException
+    {
+        try
+        {
+            if (con != null) 
+            {
+                con.close();
+                System.out.println("Conexión cerrada");
+            }
+        }
+        catch(SQLException e)
+        {
+            e.printStackTrace();
+        }
+    }
+    
+}
---
+++ src/gmgmultiverso/model/Proveedor.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gmgmultiverso.model;
+
+/**
+ *
+ * @author geanina.foanta
+ */
+public class Proveedor 
+{
+    private String nombre_empresa;
+    private int telefono;
+    private String email;
+    
+    //-----------Constructor vacio-----------
+    public Proveedor()
+    {
+
+    }
+    
+    //-----------Constructor-----------
+    public Proveedor(String nombre_empresa, int telefono, String email) 
+    {
+        this.nombre_empresa = nombre_empresa;
+        this.telefono = telefono;
+        this.email = email;
+    }
+    
+    //-----------nombre_empresa-----------
+    public String nombre_empresa() 
+    {
+        return nombre_empresa;
+    }
+
+    public void setNombre_empresa(String nombre_empresa) 
+    {
+        this.nombre_empresa = nombre_empresa;
+    }
+    
+    //-----------telefono-----------
+    public int getTelefono() 
+    {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) 
+    {
+        this.telefono = telefono;
+    }
+    
+    //-----------nombre_empresa-----------
+    public String getEmail() 
+    {
+        return email;
+    }
+
+    public void setEmail(String email) 
+    {
+        this.email = email;
+    }
+    
+    @Override
+    public String toString() 
+    {
+        return "Proveedor{" +
+                "nombre_empresa='" + nombre_empresa + '\'' +
+                ", telefono='" + telefono + '\'' +
+                ", email='" + email +
+                '}';
+    }
+}
